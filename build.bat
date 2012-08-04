d50eb3663059a6cec61a3d961163b8a3106a8eb1
Nader Chehab <naderchehab@gmail.com>
2012-08-04 13:56:49 -0400

    fixed minification

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
 "C:\Program Files (x86)\Microsoft\Microsoft Ajax Minifier\ajaxmin.exe" -clobber:true js/jquery.min.js js/midi/DOMLoader.XMLHttp.js js/midi/DOMLoader.script.js js/midi/MIDI.audioDetect.js js/midi/MIDI.loadPlugin.js js/midi/MIDI.Plugin.js js/midi/MIDI.Player.js js/midi/VersionControl.Base64.js js/midi/lib/base64binary.js js/chordbuilder.js -out min/chordbuilder.all.min.js
 "C:\Program Files (x86)\Microsoft\Microsoft Ajax Minifier\ajaxmin.exe" -clobber:true css/bootstrap.css css/chordbuilder.css -out min/chordbuilder.min.css
+copy index.html min\index.html
 pause
